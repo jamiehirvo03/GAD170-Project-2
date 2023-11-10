264e2d7274247501e5a7769961481b7e6879d06b
Jamie <hirvonenjamie@gmail.com>
2023-11-10 19:17:00 +1100

    Got random candidate generator working

--- Assets/Scripts/Crewmate.cs
+++ Assets/Scripts/Crewmate.cs
@@ -1,15 +1,36 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Crewmate : MonoBehaviour
 {
-    private bool isParasite;
-   
-    public List<string> firstNames = new List<string>(15) { "Bob", "Dougie", "Hannah", "Jenny", "Peter", "Melissa", "Hugo", "Natasha", "Timothy", "Nancy", "Johnny", "Robert", "Chloe", "Emily", "Mike" };
-    public List<string> lastNames = new List<string>(26) { "A.", "B.", "C.", "D.", "E.", "F.", "G.", "H.", "I.", "J.", "K.", "L.", "M.", "N.", "O.", "P.", "Q.", "R.", "S.", "T.", "U.", "V.", "W.", "X.", "Y.", "Z." };
-    public List<string> humanHobbies = new List<string>(5) { "Bowling", "Surfing", "Stargazing", "Kareoke", "Reading" };
-    public List<string> parasiteHobbies = new List<string>(5) { "Invasion", "Moon Fishing", "Blending In", "Sabotage", "Planetary Pool" };
+    private List<string> firstNames = new List<string>(15) { "Bob", "Dougie", "Hannah", "Jenny", "Peter", "Melissa", "Hugo", "Natasha", "Timothy", "Nancy", "Johnny", "Robert", "Chloe", "Emily", "Mike" };
+    private List<string> lastNames = new List<string>(26) { "A.", "B.", "C.", "D.", "E.", "F.", "G.", "H.", "I.", "J.", "K.", "L.", "M.", "N.", "O.", "P.", "Q.", "R.", "S.", "T.", "U.", "V.", "W.", "X.", "Y.", "Z." };
+    private List<string> humanHobbies = new List<string>(5) { "Bowling", "Surfing", "Stargazing", "Kareoke", "Reading" };
+    private List<string> parasiteHobbies = new List<string>(5) { "Invasion", "Murder", "Blending In", "Sabotage", "Fraud" };
+
+
+    public string GetFirstName()
+    {
+        return firstNames[Random.Range(0,firstNames.Count)];
+    }
+
+    public string GetLastName()
+    {
+        return lastNames[Random.Range(0,lastNames.Count)];    
+    }
+
+    public string GetHumanHobby()
+    {
+        return humanHobbies[Random.Range(0,humanHobbies.Count)];
+    }
+
+    public string GetParasiteHobby()
+    {
+        return parasiteHobbies[Random.Range(0,parasiteHobbies.Count)];
+    }
+
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,20 +6,24 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     public Crewmate crewmate;
-    public string firstName;
-    public string lastName;
-    public string candidateHobby;
-    public bool isParasite;
+    private string firstName;
+    private string lastName;
+    private string candidateHobby;
+
+    private int randomIndex; 
+    private bool isParasite;
 
 
+    //Access names from the Crewmate script and store them here
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //Access names from the Crewmate script and store them here
+        CrewmateApplication();
 
     }
+
 
     // Update is called once per frame
     void Update()
@@ -27,9 +31,40 @@ public class Ship : MonoBehaviour
     
     }
 
-    public void CrewmateApplication()
+    private void CrewmateApplication()
     {
-        firstName = crewmate.firstNames.ElementAt(1);
+        HumanOrParasite();
 
+        firstName = crewmate.GetFirstName();
+        lastName = crewmate.GetLastName();
+
+        if (isParasite)
+        {
+            candidateHobby = crewmate.GetParasiteHobby();
+        }
+        else
+        {
+            candidateHobby = crewmate.GetHumanHobby();
+        }
+
+        Debug.Log($"First Name: {firstName}");
+        Debug.Log($"Last Name: {lastName}");
+        Debug.Log($"Is a Parasite: {isParasite}");
+        Debug.Log($"Their favoruite thing is: {candidateHobby}");
+        
+    }
+    private void HumanOrParasite()
+    {
+        randomIndex = Random.Range(0, 10);
+        Debug.Log($"Random Index: {randomIndex}");
+
+        if (randomIndex > 5)
+        {
+            isParasite = true;
+        }
+        else
+        {
+            isParasite = false;
+        }
     }
 }
